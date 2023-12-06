31c90e13675a2c254f1d38e22e6846d621e93f3b
kirillEN <81442482+kixxpy@users.noreply.github.com>
2023-12-06 17:04:37 +0300

    Переписана логика useLocation на NavLink

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import Button from './components/Button/Button';
-import Input from './components/Input/Input';
 
 
 
 function App() {
 	return (
 		<>
-			<Button>Кнопка</Button>
-			<Button appeareance='big'>Кнопка</Button>
-			<Input placeholder='Email'/>
 		</>
 	);
 }
---
+++ src/layout/Layout/Layout.tsx
@@ -0,0 +1,39 @@
+import { NavLink, Outlet } from 'react-router-dom';
+import styles from './Layout.module.css';
+import Button from '../../components/Button/Button';
+import cn from 'classnames';
+
+export function Layout() {
+	return (
+		<div className={styles['layout']}>
+			<div className={styles['sidebar']}>
+				<div className={styles['user']}>
+					<img className={styles['avatar']} src="/user.png" alt="Аватар пользователя" />
+					<div className={styles['name']}>Кирилл Елисеев</div>
+					<div className={styles['email']}>ekn62@bk.ru</div>
+				</div>
+				<div className={styles['menu']}>
+					<NavLink to="/" className={({isActive}) => cn(styles['link'], {
+						[styles.action]: isActive
+					})}>
+						<img src="/menu-icon.svg" alt="Иконка меню" />
+						Меню
+					</NavLink>
+					<NavLink to="/cart" className={({isActive}) => cn(styles['link'], {
+						[styles.action]: isActive
+					})}>
+						<img src="/cart-icon.svg" alt="Иконка корзины" />
+						Корзина
+					</NavLink>
+				</div>
+				<Button className={styles['exit']}>
+					<img src="/exit-icon.svg" alt="Иконка выхода" />
+					Выйти
+				</Button>
+			</div>
+			<div>
+				<Outlet />
+			</div>
+		</div>
+	);
+}
