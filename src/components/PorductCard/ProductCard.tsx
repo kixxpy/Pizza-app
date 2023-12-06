7083ffe8129008b591ee65cafa33f244de17d8bf
kirillEN <81442482+kixxpy@users.noreply.github.com>
2023-12-06 23:12:14 +0300

    Добавил компонент карточки ProductCard

---
+++ src/components/PorductCard/ProductCard.tsx
@@ -0,0 +1,27 @@
+import styles from './ProductCard.module.css';
+import { ProductCardProps } from './ProductCard.props'
+
+
+const PorductCard = (props: ProductCardProps) => {
+	return <div className={styles['card']}>
+		<div className={styles['head']} style={{backgroundImage: `url(${props.image})`}}>
+			<div className={styles['price']}>
+				{props.price}
+				<span className={styles['currrency']}>₽</span>
+			</div>
+			<button className={styles['add-to-card']}>
+				<img src="/card-button-icon.svg" alt="Иконка добавления в корзину" />
+			</button>
+			<div className={styles['rating']}>
+				{props.rating}
+				<img src="/star-icon.svg" alt="Иконка звезды" />
+			</div>
+		</div>
+		<div className={styles['footer']}>
+			<div className={styles['title']}>{props.title}</div>
+			<div className={styles['description']}>{props.description}</div>
+		</div>
+	</div>;
+};
+
+export default PorductCard;
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,12 +1,25 @@
-import Headling from '../../components/Headling/Headling'
-import Search from '../../components/Search/Search'
-import styles from './Menu.module.css'
+import Headling from '../../components/Headling/Headling';
+import ProductCard from '../../components/PorductCard/ProductCard';
+import Search from '../../components/Search/Search';
+import styles from './Menu.module.css';
 
 export function Menu() {
 	return (
-		<div className={styles['head']}>
-			<Headling>Меню</Headling>
-			<Search placeholder='Введите блюдо или состав'/>
-		</div>
-	)
+		<>
+			<div className={styles['head']}>
+				<Headling>Меню</Headling>
+				<Search placeholder="Введите блюдо или состав" />
+			</div>
+			<div>
+				<ProductCard
+					id={1}
+					rating={4.5}
+					title="Наслаждение"
+					description="Салями, руккола, помидоры, оливки"
+					price={300}
+					image='/product-demo.png'
+				/>
+			</div>
+		</>
+	);
 }
