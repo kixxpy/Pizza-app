f41ba55707cda44fba75e757d1003712079dc138
kirillEN <81442482+kixxpy@users.noreply.github.com>
2023-12-06 23:33:14 +0300

    Добавил логику проваливания в каточку товара

--- src/components/PorductCard/ProductCard.tsx
+++ src/components/PorductCard/ProductCard.tsx
@@ -1,27 +1,31 @@
+import { Link } from 'react-router-dom'
 import styles from './ProductCard.module.css';
-import { ProductCardProps } from './ProductCard.props'
-
+import { ProductCardProps } from './ProductCard.props';
 
 const PorductCard = (props: ProductCardProps) => {
-	return <div className={styles['card']}>
-		<div className={styles['head']} style={{backgroundImage: `url(${props.image})`}}>
-			<div className={styles['price']}>
-				{props.price}
-				<span className={styles['currrency']}>₽</span>
+	return (
+		<Link to={`/product/${props.id}`} className={styles['link']}>
+			<div className={styles['card']}>
+				<div className={styles['head']} style={{ backgroundImage: `url(${props.image})` }}>
+					<div className={styles['price']}>
+						{props.price}
+						<span className={styles['currrency']}>₽</span>
+					</div>
+					<button className={styles['add-to-card']}>
+						<img src="/card-button-icon.svg" alt="Иконка добавления в корзину" />
+					</button>
+					<div className={styles['rating']}>
+						{props.rating}
+						<img src="/star-icon.svg" alt="Иконка звезды" />
+					</div>
+				</div>
+				<div className={styles['footer']}>
+					<div className={styles['title']}>{props.title}</div>
+					<div className={styles['description']}>{props.description}</div>
+				</div>
 			</div>
-			<button className={styles['add-to-card']}>
-				<img src="/card-button-icon.svg" alt="Иконка добавления в корзину" />
-			</button>
-			<div className={styles['rating']}>
-				{props.rating}
-				<img src="/star-icon.svg" alt="Иконка звезды" />
-			</div>
-		</div>
-		<div className={styles['footer']}>
-			<div className={styles['title']}>{props.title}</div>
-			<div className={styles['description']}>{props.description}</div>
-		</div>
-	</div>;
+		</Link>
+	);
 };
 
 export default PorductCard;
